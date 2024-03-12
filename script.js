c343e5a4704350517004348de4bee61dc8f40ac9
Sam Burgett <samwise.burgett@outlook.com>
2024-03-12 11:39:59 -0400

    combining all the functions into one functions I wanted to keep them in separate pieces but I am having trouble getting it to work like that

--- script.js
+++ script.js
@@ -5,37 +5,41 @@ const result = document.getElementById("result");
 // unicode 0061-007A
 
 //checks if input box is empty
-function empty() {
-    if (textInput.value.length <= 0) {
-        const emptyInput = "Please input a value"
-        alert(emptyInput)
-    }
-}
+// function empty() {
+//     if (textInput.value.length <= 0) {
+//         const emptyInput = "Please input a value"
+//         alert(emptyInput)
+//     }
+// }
 
-//converts input value to lowercase letters
-function lowercase() {
-    const lowerString = textInput.value.toLowerCase();
-    return lowerString
-}
+// //converts input value to lowercase letters
+// function lowercase() {
+//     const lowerString = textInput.value.toLowerCase();
+//     return lowerString
+// }
 
-function arrayify() {
-    const stringArray = Array.from(lowercase())
-    console.log(stringArray);
-    return stringArray
-} //yay this one works
+// function arrayify() {
+//     const stringArray = Array.from(lowercase())
+//     console.log(stringArray);
+//     return stringArray
+// } //yay this one works
 
-function resultDisplay() {
-    if (arrayify() === arrayify().reverse) {
-        const result = result.innerHTML(`<div>${textInput.value} is a palindrome</div>`)
-        
-    }
-}
+// function resultDisplay() {
+//     if (arrayify() === arrayify().reverse) {
+//         const result = result.innerHTML(`<div>${textInput.value} is a palindrome</div>`)
+//     }
+// }
 
 //functions to run on button click
 function palindromeCheck() {
-    empty();
-    lowercase();
-    arrayify();
-    resultDisplay();
-}
+    if (textInput.value.length <= 0) {
+        const emptyInput = "Please input a value"
+            alert(emptyInput)
+    }
+    const lowerString = textInput.value.toLowerCase();
+    const stringArray = Array.from(lowerString);
+    if (stringArray === stringArray.reverse()) {
+        console.log("palindrome")
+    }
 
+}
