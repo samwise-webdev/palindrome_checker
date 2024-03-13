62afa35bb4f64cf0b4ee161d1453b53e585c0978
Sam Burgett <samwise.burgett@outlook.com>
2024-03-13 09:57:50 -0400

    corrected array always being reversed corrected comparison by turning arrays back into strings

--- script.js
+++ script.js
@@ -5,43 +5,24 @@ const result = document.getElementById("result");
 // unicode 0061-007A
 
 //checks if input box is empty
-// function empty() {
-//     if (textInput.value.length <= 0) {
-//         const emptyInput = "Please input a value"
-//         alert(emptyInput)
-//     }
-// }
-
-// //converts input value to lowercase letters
-// function lowercase() {
-//     const lowerString = textInput.value.toLowerCase();
-//     return lowerString
-// }
-
-// function arrayify() {
-//     const stringArray = Array.from(lowercase())
-//     console.log(stringArray);
-//     return stringArray
-// } //yay this one works
-
-// function resultDisplay() {
-//     if (arrayify() === arrayify().reverse) {
-//         const result = result.innerHTML(`<div>${textInput.value} is a palindrome</div>`)
-//     }
-// }
-
-//functions to run on button click
-function palindromeCheck() {
+function empty() {
     if (textInput.value.length <= 0) {
         const emptyInput = "Please input a value"
-            alert(emptyInput)
+        alert(emptyInput)
     }
-    const lowerString = textInput.value.toLowerCase();
-    const stringArray = Array.from(lowerString);
-    if (stringArray === stringArray.reverse()) {
+}
+
+//functions to run on button click
+    // using === doesn't compare contents of an array
+function palindromeCheck() {
+    empty();
+    const stringArray = Array.from(textInput.value.toLowerCase());
+    const stringReversed = Array.from(stringArray).reverse(); //need to reverse stringArray for some reason, not use textInput.value again. That didn't work, idk why
+
+    if (stringArray.join('') === stringReversed.join('')) {
         console.log("palindrome")
     } else {
-        textInput.innerHTML = `${lowerString} is not a palindrome.`
+        console.log(`${stringArray.join('')} is not a palindrome.`); //have to .join() again b/c it will show up as an array otherwise
     }
 
 }
