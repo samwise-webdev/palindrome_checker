1bb5e98d2e9208ccf48d2e1131081a1e9549e8cb
Sam Burgett <samwise.burgett@outlook.com>
2024-03-29 22:19:14 -0400

    regex removed capital letters because it runs before the toLowerCase(), so I'm going to try to put it on the regex instead and see how that goes.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const textInput = document.getElementById("text-input");
 const checkBtn = document.getElementById("check-btn");
 const result = document.getElementById("result");
-const textValue = textInput.value;
+// const textValue = textInput.value;
 // unicode 0061-007A
 
 //checks if input box is empty
@@ -28,14 +28,13 @@ function empty() {
 function palindromeCheck() {
     empty();
 //    punctRemover();
-    const punctfinder = /[^a-z0-9]/g;
-    const cleanInput = textValue.replaceAll(punctfinder, "");
+    const cleanInput = textInput.value.replaceAll(/[^a-zA-Z0-9]/g, "");
     const stringArray = Array.from(cleanInput.toLowerCase());
     const stringReversed = Array.from(cleanInput).reverse(); //need to reverse stringArray for some reason, not use textInput.value again. That didn't work, idk why
         if (stringArray.join('') === stringReversed.join('')) {
-            result.innerText = (`${stringArray.join('')} is a palindrome.`);
+            result.innerText = (`${textInput.value} is a palindrome.`);
         } else {
-            result.innerText = (`${stringArray.join('')} is not a palindrome.`); //have to .join() again b/c it will show up as an array otherwise
+            result.innerText = (`${textInput.value} is not a palindrome.`); //have to .join() again b/c it will show up as an array otherwise
         }
 
 }
